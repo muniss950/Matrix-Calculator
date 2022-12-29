67b8799073cf1b1aac55dfdc641dc748805f660c
muniss950 <114894596+muniss950@users.noreply.github.com>
2022-12-29 21:34:51 +0530

    added more functions

--- fletmat.py
+++ fletmat.py
@@ -1,38 +1,65 @@
 import flet as ft
+import matrix
 
-def yinput(c):
-        y=[]
-        for i in range(1,c+1):
-            y.append(
-                ft.Text(value='0')
-            )
-        return y
-def xinput(r,c):
-        items = []
-        items.append(ft.Text(value="\n"))
-        for i in range(1, r + 1):
-            items.append(
-            ft.Row(controls=yinput(c)))
+def lismat(l):          #to make a matrix when a list is given 
+    items = []
+    items.append(ft.Text(value="\n"))
+    for i in range(len(l)):
+            row=[]
+            for j in l[i]:
+                row.append(
+                    ft.Text(value=str(j))
+                )
             
-        return items
-
+            items.append(
+            ft.Row(controls=row))
+     
+    return items
 
 class fletmat(ft.UserControl):
-    def __init__(self,row,column):
+    def __init__(self,l=[[0,0,0],[0,0,0],[0,0,0]]):
         super().__init__()
-        self.row=row
-        self.column=column
+        self.row=len(l[0])
+        self.column=len(l)
+        self.matrix=matrix.matrix(l)
+        self.list=l
     def build(self,):
         return ft.Row(
         controls=[
             ft.Text(value='[',size=40*(self.row),weight="w100"),
             ft.Column(
                 controls=
-                xinput(self.row,self.column),
+                lismat(self.list),
         
         ),ft.Text(value=']',size=40*(self.row),weight="w100")],
     scroll=True,
 
     )
-
-
+    def getmatrix(self,):
+        return self.matrix
+    def printmat(self,):
+        print(self.matrix)
+    def lenrow(self,):
+        return len(self.list[0])
+    def lencol(self,):
+        return len(self.list)
+    def changeval(self,x,y,inp):
+        a=self.getmatrix()
+        
+        self.list[x][y]=inp
+        
+        self.update()
+        super().update()
+        return fletmat(self.list)
+    def matadd(self,rmat):
+        a=self.getmatrix()
+        b=rmat.getmatrix()
+        c=a+b
+        d=matrix.matrix.mat_getlist(c)
+        return fletmat(d)
+    def matsub(self,rmat):
+        a=self.getmatrix()
+        b=rmat.getmatrix()
+        c=a-b
+        d=matrix.matrix.mat_getlist(c)
+        return fletmat(d)
