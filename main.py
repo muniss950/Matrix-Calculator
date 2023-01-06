6c59826fa5f26f4bc34cac91f7d35a1b217b102b
muniss950 <114894596+muniss950@users.noreply.github.com>
2023-01-06 15:47:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
 import fletmat
 
+    
 def main(page: ft.Page):
 
     page.appbar = ft.AppBar(
@@ -12,58 +13,64 @@ def main(page: ft.Page):
         )
     page.title="Matrix"
     page.window_height=500
-    page.window_width=350
+    page.window_width=500
     page.window_left=500
     page.window_top=200
     page.window_resizable=False
     page.scroll=True
-    result=fletmat.fletmat(5,5)
+    #b=fletmat.fletmat([[1,1],[0,0]])
+    #c=fletmat.fletmat([[2,2],[1,1]])
+    result=fletmat.fletmat()
+    x=ft.Row(controls=[result])
+    #print(fletmat.fletmat.getcursor(result))
+    '''def x(a):
+        s=ft.Row(controls=[ft.Text(value="m")]) 
+        page.add(s)
+        page.update()'''
+    
+    
+     
+   
+    #fletmat.fletmat.printmat(result) #//to print on terminal
+    #print(fletmat.fletmat.lenrow(result))
+    def plus(a):
+        x.controls.append(ft.Text(value="+"))
+        m2=fletmat.fletmat()
+        x.controls.append(m2)
+        page.update()
     page.add(
         
-        result,
+        x,
+        ft.Row(controls=[
+                
+                enter:=ft.TextField(
+                label="Enter here",
+                border_color="transparent",
+                    keyboard_type="number",
+                #on_submit=click
+                ),
+                
+                ],),
         ft.Row(
             controls=[
-                ft.ElevatedButton(text="AC"),
-                ft.ElevatedButton(text="INV"),
-                ft.ElevatedButton(text="Tr"),
-                ft.ElevatedButton(text="S/o"),
+                ft.ElevatedButton(text="AC",expand=True),
+                ft.ElevatedButton(text="INV",expand=True),
+                ft.ElevatedButton(text="Tr",expand=True),
+                ft.ElevatedButton(text="Sp.O",expand=True),
             ],
-
+     
         ),
         ft.Row(
-            controls=[
-                ft.ElevatedButton(text="7"),
-                ft.ElevatedButton(text="8"),
-                ft.ElevatedButton(text="9"),
-                ft.ElevatedButton(text="x"),
-            ],
+            controls=[    
+                ft.ElevatedButton(text="x",expand=True),
+                ft.ElevatedButton(text="-",expand=True),
+                ft.ElevatedButton(text="+",expand=True,on_click=plus),
+                ft.ElevatedButton(text="=",expand=True,),
+                ],
+            ),
+        
 
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="4"),
-                ft.ElevatedButton(text="5"),
-                ft.ElevatedButton(text="6"),
-                ft.ElevatedButton(text="-"),
-            ],
-
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="1"),
-                ft.ElevatedButton(text="2"),
-                ft.ElevatedButton(text="3"),
-                ft.ElevatedButton(text="+"),
-            ],
-
-        ),
-        ft.Row(
-             controls=[
-                ft.ElevatedButton(text="0"),
-                ft.ElevatedButton(text="."),
-                ft.ElevatedButton(text="="),
-            ],
-
-        ),
-    )
+        )
+    
+    
 ft.app(target=main)
