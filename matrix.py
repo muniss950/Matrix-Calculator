89edf45949a2ec8077a0a857f311986b680f4b78
muniss950 <114894596+muniss950@users.noreply.github.com>
2023-01-18 19:10:55 +0530

    added _mul__ and __transpose

--- matrix.py
+++ matrix.py
@@ -26,12 +26,19 @@ class matrix(object):
             raise ValueError("Number of elements not unifrom")
         for i in range(len(self.a)):
             self.a[i].insert(y,e[i]) 
-    def checksquare(self):
+    def is_square(self):
         if len(self.a)==len(self.a[0]):
             return True
         else:
             return False
-    
+    def transpose(self):
+        res=matrix([[0 for j in range(len(self.a))] for i in range(len(self.a[0]))])
+        #print(res)
+        for i in range(len(self.a)):
+            for j in range(len(self.a[i])):
+                #print(i,j)
+                res.a[j][i]=self.a[i][j]
+        return res         
     
     def __add__(self,rmat):      #special function for defining addition
         c=rmat.mat_getlist()
@@ -51,7 +58,23 @@ class matrix(object):
         t=matrix(b)
         return t
 
-    
+    def __mul__(self,rmat):
+        c=rmat.mat_getlist()
+        b=self.mat_getlist()
+        if len(c[0])==len(b):
+            res=([[0 for j in range(len(b)) ]for i in range(len(c[0]))])
+            #print(res)
+            d=rmat.transpose().mat_getlist()
+            for row_self in range(len(b)):
+                for row_transpose in range(len(d)):
+                    new_element=0
+                    for column_self in range(len(b[0])):
+                        new_element+=(b[row_self][column_self]*d[row_transpose][column_self])
+                    #print(row_self,row_transpose)
+                    res[row_self][row_transpose]=new_element
+            return matrix(res)
+        else:
+            raise ValueError("no of columns in first matrix must be equal to no of rows in second matrix")    
     
     def __str__(self):   #for printing matrix
 
@@ -64,4 +87,4 @@ class matrix(object):
                 x+="\n"
             return x
 
-        return s(self.a)       
+        return s(self.a)    
